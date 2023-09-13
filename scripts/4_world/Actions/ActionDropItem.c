5ca0a983b2fe3265bc08b4116d7ece64b934ceef
TRG-SN1P3R <121766449+TRG-SN1P3R@users.noreply.github.com>
2023-09-13 14:57:50 -0500

    Update

--- scripts/4_world/Actions/ActionDropItem.c
+++ scripts/4_world/Actions/ActionDropItem.c
@@ -4,10 +4,15 @@ modded class ActionDropItem : ActionSingleUseBase{
 		super.OnExecuteServer(action_data);
 
 		if(!action_data.m_Player || !action_data.m_Target) return;//Check for NULL
-
+		string item;
 		if(m_LogConfig.ServerConfig.ShowDropStorage==0) return;//Checks if we want to see this action
 
-		string item = action_data.m_MainItem.GetType(); //Gets item name
+		if(m_LogConfig.ServerConfig.SimpleLogsStorage==1){ //Do we want to see the Entity ID
+			item = action_data.m_MainItem.GetType(); //Gets item name
+		}
+		else{
+			item = string.Format("%1",action_data.m_MainItem.GetParent());
+		}
 
 		foreach(string CheckItem: m_LogConfig.CustomConfig.DropMonitorItems){
 			if(CheckItem=="") return;//leaves the loop if there is bad data.
