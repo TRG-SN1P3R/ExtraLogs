fb83204f1b85a97c47e356fdabb49464a84aa464
TRG-SN1P3R <121766449+TRG-SN1P3R@users.noreply.github.com>
2023-09-13 10:10:27 -0500

    Update

--- scripts/4_world/Actions/ActionAttachToConstruction.c
+++ scripts/4_world/Actions/ActionAttachToConstruction.c
@@ -4,16 +4,29 @@ modded class ActionAttachToConstruction: ActionSingleUseBase {
 		super.OnStartServer(action_data);
 			if(!action_data.m_Player || !action_data.m_Target) return;
 			if(m_LogConfig.ServerConfig.ShowLockAttach==0) return; //checks to see if we want to see this
+
+
 			string check=action_data.m_MainItem.GetType(); //Gets item name to gate logic
-			string message = action_data.m_MainItem.GetType()+" attached to gate"; //sting to pass to the place log type
+			string message;
+			CombinationLock lock;
+
+
+			if(m_LogConfig.ServerConfig.SimpleLogs==1){ //Return Lock without Entity ID
+
+			message = action_data.m_MainItem.GetType()+" attached to gate"; //sting to pass to the place log type
+			}
+			else{
+				message=action_data.m_MainItem.GetParent(); //Return Locks Entity ID
+			}
 			
 				if(check=="CombinationLock"){ //3Dial
 					if(m_LogConfig.ServerConfig.ShowLockCode==0){
 						SendToCFToolsPlace(action_data.m_Player,message);
 						return;
 					}
-					CombinationLock lock = CombinationLock.Cast(action_data.m_MainItem);
-					if(!lock) SendToCFToolsPlace(action_data.m_Player,message+" error");
+
+					lock = CombinationLock.Cast(action_data.m_MainItem);//Take Item to clas CombinationLock to get COMBO
+					if(!lock) return;
 					SendToCFToolsPlace(action_data.m_Player,string.Format("%1 with COMBO:%2",message,lock.GetCombination()));
 					return;
 				}	
@@ -23,8 +36,9 @@ modded class ActionAttachToConstruction: ActionSingleUseBase {
 						SendToCFToolsPlace(action_data.m_Player,message);
 						return;
 					}
-					CombinationLock lock2 = CombinationLock.Cast(action_data.m_MainItem);
-					if(!lock2) SendToCFToolsPlace(action_data.m_Player,message+" error");
+
+					lock = CombinationLock.Cast(action_data.m_MainItem);//Take Item to clas CombinationLock to get COMBO
+					if(!lock) return;
 					SendToCFToolsPlace(action_data.m_Player,string.Format("%1 with COMBO:%2",message,lock2.GetCombination()));
 					return;
 				}
